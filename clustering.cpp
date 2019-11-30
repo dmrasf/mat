9292611e957891ae22efc0e675537017815b78d1
MR D <dmrasf@qq.com>
2019-12-01 00:54:23 +0800

    修改k-means的错误，并添加注释

--- clustering.cpp
+++ clustering.cpp
@@ -30,14 +30,17 @@ void Clustering::train(const MatrixXd &x_train, const MatrixXd &y_train, int n, 
 void Clustering::k_means(const MatrixXd &x_train, int n){
 	MatrixXd sum = clas;
 	VectorXd cla_num(clas.cols());
+	//n:训练次数 
 	while(n--){
 		sum.setZero();
 		cla_num.setZero(); 
+		//对每个数据分类 
 		for(int i = 0; i != x_train.cols(); i++){
 			int min_cla = find_min(x_train.col(i));
 			sum.col(min_cla) = sum.col(min_cla).array() + x_train.col(i).array();
 			cla_num(min_cla) = cla_num(min_cla) + 1;
-		} 
+		}
+		//利用分好类求新的中心向量 
 		for(int i = 0; i != clas.cols(); i++){
 			if(cla_num(i) != 0)
 				clas.col(i) = sum.col(i)/cla_num(i);
@@ -49,27 +52,26 @@ void Clustering::lvq(const MatrixXd &x_train, const MatrixXd &y_train, int n){
 			
 	}
 }
-
+//寻找最近的中心向量的函数 
 int Clustering::find_min(const VectorXd &x_test){
 	MatrixXd len = clas;
-	VectorXd u;
+	VectorXd u; 
 	for(int i = 0; i != len.cols(); i++)
 		len.col(i) = len.col(i).array() - x_test.array();
+	//求差 然后平方和 
 	len = len.array().square();
 	u = len.colwise().sum();
 	MatrixXd::Index minRow, minCol;
 	u.minCoeff(&minRow,&minCol);
 	return minRow;
 }
- 
+//预测函数 
 VectorXd Clustering::predict(const MatrixXd &x_test){
-	cout << "x_test = " << x_test << endl;
 	VectorXd y_pre(x_test.cols());
 	y_pre.setZero();
-	cout << "y = " << y_pre << endl;
+	//对要预测的每个数据寻找最近的中心向量 得到的第n类 用k(n)返回预测值 
 	for(int i = 0; i != x_test.cols(); i++){
-		y_pre(i) = find_min(x_test.col(i));
-		cout << y_pre(i) << endl; 
+		y_pre(i) = k(find_min(x_test.col(i)));
 	}
 	return y_pre;
 } 
