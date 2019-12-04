8ef0b29ec413ae145678c1b565b9253f91aebf7d
MR D <dmrasf@qq.com>
2019-12-04 23:21:53 +0800

    添加核函数

--- svm.cpp
+++ svm.cpp
@@ -1,6 +1,35 @@
 #include "svm.h"
 
-Svm::Svm(){
+
+Svm::Svm(const MatrixXd &x, const MatrixXd &y){
+	
+} 
+
+
+//求第i个数据的a 
+double Svm::solve_a(int i){
+	VectorXd a_t = a;
+	//固定其他值 
+	a_t.setOnes();
+	a_t(i) = 0;
+	double ai = 0;
+	//输入数据量  m个 
+	int m = x.cols();
+	double a_sum = 0;
+	for(int i = 0; i != m; i++){
+		a_sum +=  
+	} 
+}
+
+
+
+
+
+double Svm::Gaussian_kernel(const VectorXd &xi, const VectorxXd &xj){
+	VectorXd temp = (xi.array() - xj.array()).square().sum();
+	temp = temp.array().exp()/(-2*1);
+	return temp(0);
+	
 }
 
 Svm::~Svm(){
--- svm.h
+++ svm.h
@@ -11,10 +11,22 @@ using namespace std;
 class Svm
 {
 public:
-	Svm();
+	Svm(const MatrixXd&, const MatrixXd&); 
+	//高斯核函数 
+	double Gaussian_kernel(const VectorXd&, const VectorXd&);
+	
+	void train(); 
+	//求a  SMO
+	double solve_a(int); 
 	~Svm();
 private:
-	
+	//不为0的a  
+	VectorXd a;
+	MatrixXd y;
+	//支持向量 
+	MatrixXd x;
+	//常数  软间隔   需要尝试最好的 
+	int C; 
 };
 
 #endif
